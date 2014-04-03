c67923bb9460801ac7ccbac2fc08ab7781ce1ce0
crox <ivg@ieee.org>
2014-04-03 09:53:25 +0400

    a preliminary version of an AST.
    Just to start from something.

--- src/ast.ml
+++ src/ast.ml
@@ -1,1 +1,31 @@
-type t
+(** A definition of abstract syntax tree. *)
+
+open Core.Std
+
+(** binary operations  *)
+type binary =
+  | Mul        (** an overloaded multiplication  *)
+  | Sub        (** subtraction  *)
+  | Add        (** Addition  *)
+  | Pow        (** Power *)
+  | Had        (** Hadamard multiplication  *)
+with sexp
+
+(** unary operations  *)
+type unary =
+  | Tran      (** Transpose (and possibly conjugate)   *)
+  | Conj      (** Conjugate (without a transposition)  *)
+  | UNeg      (** Negation  *)
+with sexp
+
+
+(** a type to denote matrix indices  *)
+type index = Num of int | Sym of string with sexp
+
+type t =
+  | Var of string              (** A variable        *)
+  | Uop of unary  * t          (** Unary operation   *)
+  | Bop of binary * t * t      (** Binary operation  *)
+  | Sub of t * index list      (** Indexing  *)
+with sexp
+
