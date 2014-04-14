4a8cbb7c9aef5c373469f41aee909edd0d5bada7
crox <ivg@ieee.org>
2014-04-14 14:06:09 +0400

    added expression builders, to simplify futher development.

---
+++ src/exp.mli
@@ -0,0 +1,17 @@
+(** Helpers to build expression.
+    Example:
+      Expression $Ax = b$, can be encoded with:
+      [Exp.(Var 'A' * Var 'x' + Var 'b')]
+  *)
+
+type t = Ast.t
+
+val var: char -> t
+val ( * ): t -> t -> t
+val ( + ): t -> t -> t
+val ( - ): t -> t -> t
+val ( *. ): t -> t -> t
+val ( ** ): t -> t -> t
+val tran: t -> t
+val conj: t -> t
+val to_string: t -> string
