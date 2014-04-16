8c281b98eb274fcf8bdc23594e62ffb2f3f374a8
Ivan Gotovchits <ivg@ieee.org>
2014-04-16 22:45:45 +0400

    basic support for environment. wip.

--- src/env.ml
+++ src/env.ml
@@ -1,3 +1,42 @@
-type t = unit
+open Core.Std
+open Ast
+open Type
 
-let empty () = ()
+(** Environment is a symbol table *)
+module Table = Sym.Table
+type t = ty Table.t with sexp
+
+
+let empty () : t = Table.create ()
+
+let min_sym = 'a'
+
+(** a stub to generate fresh type variables  *)
+let next_char char : char option =
+  match (Char.to_int char + 1) |> Char.of_int with
+  | Some c when Char.(is_lowercase c && is_alpha c) -> Some char
+  | _ -> None
+
+let next_sym sym : sym =
+  match Sym.to_list sym |> List.rev with
+  | [] -> assert false
+  | t::hs -> match next_char t with
+    | Some t -> List.rev (t::hs) |> Sym.of_char_list
+    | None   -> min_sym :: t :: hs |> List.rev |> Sym.of_char_list
+
+let add_fresh (env : t) sym : ty =
+  let fresh_var =
+    let max_var =
+      Table.fold env ~init:(Sym.of_char min_sym)
+        ~f:(fun ~key:_ ~data max_sym -> match data with
+            | TVar id -> if id < max_sym then max_sym else id
+            | _ -> (max_sym : sym)) in
+    next_sym max_var in
+  let ty = TVar fresh_var in
+  Table.add_exn env ~key:sym ~data:ty;
+  ty
+
+let get_or_add_fresh (env:t) sym : ty =
+  match Table.find env sym with
+  | Some ty -> ty
+  | None -> add_fresh env sym
--- src/env.mli
+++ src/env.mli
@@ -1,3 +1,16 @@
+(** Context is a set of bindings from symbols to types  *)
+open Ast
+open Type
+
 type t
 
 val empty: unit -> t
+
+val add_fresh: t -> sym -> ty
+(** [add_fresh ctx sym] creates a fresh type variable and bind it
+    with identifier [sym]. Return the freshly created variable *)
+
+val get_or_add_fresh: t -> sym -> ty
+(** [get_or_add_fresh ctx id] if [id] is bound in [ctx] then return
+    its type, else create a fresh variable, bound it to [id] and
+    return it  *)
