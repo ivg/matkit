340a0513150f8b25fd0f7087fe8851ae38fb6d17
Thomas Leu <thomasleu@g.harvard.edu>
2014-04-19 10:26:10 -0400

    Updated testing functionality to use exp.ml

--- src/exp.mli
+++ src/exp.mli
@@ -14,6 +14,7 @@ val ( + ): t -> t -> t
 val ( - ): t -> t -> t
 val ( *. ): t -> t -> t
 val ( ** ): t -> t -> t
+val neg: t -> t
 val tran: t -> t
 val conj: t -> t
 val to_string: t -> string
--- src/tests/test_parsing.ml
+++ src/tests/test_parsing.ml
@@ -5,17 +5,74 @@ open Ast
 type parser_output = (exp option * (char * string) list) list
 with sexp
 
+(*** PARSE, EXTRACT, PRINT AST ***)
 let string_of_parser_output out =
   Sexp.to_string (sexp_of_parser_output out)
+  
+let parse x = Parser.script Lexer.tokens (Lexing.from_string x)
 
+let extract_exp (x: parser_output)  = 
+  match x with
+  | [] | (None, _) :: _ -> failwith "No representation for empty exp\n"
+  | (Some e, _) :: _ -> e
 
-let test_data (data: string list) =
-  List.iter ~f:(fun x ->
-    Parser.script Lexer.tokens (Lexing.from_string x)
-    |> string_of_parser_output
-    |>printf "%s\n") data
+let print_parse data =
+  parse data
+  |> string_of_parser_output
+  |>printf "%s\n"
+  
     
+(*** TEST PARSING HELPERS ***)
+let assert_parse e str =
+  assert(e = extract_exp (parse str))
+  
+let test_parse e str =
+  print_parse str;
+  assert_parse e str
+  
+let test_parses (input: (exp * string) list) =
+  List.iter ~f:(fun (e,s) -> test_parse e s) input
+  
+    
+(*** TESTING FUNCTIONS ***)
 let test_term () =
+  printf "*** Testing Single Terms ***\n";
+  test_parses [ 
+    (Exp.(Var "A"), "A.");
+    (Exp.(Var "a"), "a.");
+    (Exp.(Num (-1)), "-1.");
+    (Exp.(Var "A"), "(A).")
+  ]
+  
+let test_unop () =
+  printf "*** Testing Unary Operations ***\n";
+  test_parses [
+    (Exp.(neg (Var "A")), "~A.");
+    (Exp.(tran (Var "A")), "A'.");
+    (Exp.(tran (neg (Var "A"))), "~A'.");
+    (Exp.(neg (tran (Var "A"))), "~(A').")
+  ]
+
+let test_binop () =
+  printf "*** Testing Binary Operations ***\n";
+  test_parses [
+    (Exp.(Var "A" + Var "B"), "A+B.");
+    (Exp.(Var "A" - Var "B"), "A-B.");
+    (Exp.(Var "A" * Var "B"), "A*B.");
+    (Exp.(Var "A" *. Var "B"), "A.*B.");
+    (Exp.(Var "A" + (Var "B" * Var "C")), "A + B * C.");
+    (Exp.(Var "A" * Var "B" + Var "C"),  "A * B + C.")
+  ]
+    
+let test_combinations () =
+  printf "*** Testing Combined Operations ***\n";
+  test_parses [
+    (Exp.(neg (Var "A") + Var "B"), "~A+B.");
+    (Exp.(neg (Var "A") + (neg (Var "B"))), "~A+~B.");
+    (Exp.(tran (neg (Var "A")) * (tran (neg (Var "B")))), "~A'*~B'.")
+  ]
+  
+(*let test_term () =
   printf "*** Testing Single Terms ***\n";
   test_data [
     "1.";
@@ -24,7 +81,7 @@ let test_term () =
     "(a).";
   ]
   
-let test_unop () =
+  let test_unop () =
   printf "*** Testing Unary Operations ***\n";
   test_data [
     "~1.";  
@@ -52,12 +109,13 @@ let test_binop () =
     "~a * b.";
     "~(a - b) * c.";
     "~(4 * a) - (b + c)."
-  ]
+  ]*)
 
 let run_tests () =
   test_term ();
   test_unop ();
-  test_binop ()
+  test_binop ();
+  test_combinations ()
 
 let () = run_tests ()
 
